26949f0f52834a3ed27e6c56c66f47a17a41a64c
Chizihn <chizihn@gmail.com>
2025-04-24 06:31:56 +0100

    tired

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,2 @@
-import express from "express";
-const app = express();
-
-// Add a route that logs requests
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
-
-// Simple test route
-app.get("*", (req, res) => {
-  res.status(200).json({
-    message: "Debug route working",
-    path: req.path,
-    query: req.query,
-    method: req.method,
-  });
-});
-
+import app from "../src/index";
 export default app;
