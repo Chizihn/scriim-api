98334da904758d3cf2a6af18f11cec1c26d023c4
Chizihn <chizihn@gmail.com>
2025-04-24 06:16:20 +0100

    again

--- api/index.ts
+++ api/index.ts
@@ -1,2 +1,9 @@
-import app from "../src/index";
+// api/index.ts
+import express from "express";
+const app = express();
+
+app.get("/*", (req, res) => {
+  res.json({ status: "working", path: req.path });
+});
+
 export default app;
